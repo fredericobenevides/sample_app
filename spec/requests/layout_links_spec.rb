f16e436aa532396698ddad7d1b2e63942fccc2e0
Frederico Benevides <fredbene@gmail.com>
2011-11-15 15:20:03 -0200

    Editing and updating users

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -67,13 +67,19 @@ describe "LayoutLinks" do
     it "should have a signout link" do
       visit root_path
       response.should have_selector("a", :href => signout_path,
-                                        :content => "Sign out")
+                                         :content => "Sign out")
     end
 
     it "should have a profile link" do
       visit root_path
       response.should have_selector("a", :href => user_path(@user),
-                                        :content => "Profile")
+                                         :content => "Profile")
+    end
+
+    it "should have a settings link" do
+      visit root_path
+      response.should have_selector('a', :href => edit_user_path(@user),
+                                        :content => "Settings")
     end
   end
 end
