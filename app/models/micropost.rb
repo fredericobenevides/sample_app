3b27a464ebc6c8b7cc23fbd2dec39faff382bb99
Frederico Benevides <fredbene@gmail.com>
2011-11-20 02:11:46 -0200

    Made a working feed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -18,4 +18,18 @@ class Micropost < ActiveRecord::Base
   validates :user_id, :presence => true
 
   default_scope :order => 'microposts.created_at DESC'
+
+  scope :from_users_followed_by, lambda { |user| followed_by(user)}
+
+ private
+
+  class << self
+    def followed_by(user)
+      #followed_ids = user.following.map(&:id).join(", ")
+      followed_ids = %(SELECT followed_id FROM relationships
+                       WHERE follower_id = :user_id)
+      where("user_id IN (#{followed_ids}) OR user_id = :user_id",
+            :user_id => user)
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -222,8 +222,15 @@ describe User do
       end
 
       it "should not include a differente user's micropost" do
-        @mp3 = Factory(:micropost, :user => Factory(:user, :email => Factory.next(:email)))
-        @user.feed.should_not include(@mp3)
+        mp3 = Factory(:micropost, :user => Factory(:user, :email => Factory.next(:email)))
+        @user.feed.should_not include(mp3)
+      end
+
+      it "should include the microposts of followed users" do
+        followed = Factory(:user, :email => Factory.next(:email))
+        mp3 = Factory(:micropost, :user => followed)
+        @user.follow!(followed)
+        @user.feed.should include(mp3)
       end
     end
   end
